54225332ece37d2ea3559e6ffdef99d733507c5f
long.woo <longwunet@outlook.com>
2022-01-15 21:57:37 +0800

    feat: 增加类型转换

--- src/definition.ts
+++ src/definition.ts
@@ -1,4 +1,10 @@
-import { IDefaultObject, ISwaggerResultDefinitions } from "./swagger.ts";
+import Logs from "./console.ts";
+import {
+  IDefaultObject,
+  ISwaggerDefinitionPropertiesItems,
+  ISwaggerResultDefinitions,
+  propertyType,
+} from "./swagger.ts";
 
 /**
  * 获取定义的名称
@@ -35,17 +41,82 @@ const getDefinitionName = (name: string, isDefinition?: boolean): string => {
   return newName;
 };
 
-const getDefinitionContent = (): string => {
+/**
+ * 转换为 TypeScript 类型
+ * @param type - 属性基础类型
+ * @param typeItem - 属性非基础类型
+ * @param defKey - 定义源中的 key
+ * @returns
+ */
+const convertType = (
+  type: string,
+  typeItem?: ISwaggerDefinitionPropertiesItems,
+): string => {
+  switch (type) {
+    case "integer":
+      return "number";
+    case "array":
+      if (typeItem?.type) {
+        const childType = convertType(typeItem.type);
+
+        return `${childType}[]`;
+      }
+
+      if (typeItem?.$ref) {
+        const name = getDefinitionName(typeItem.$ref);
+        const value = `${name}[]`;
+
+        return value;
+      }
+
+      return "[]";
+    case "object": {
+      return "IDefaultObject";
+    }
+    default:
+      // 自定义类型
+      if (type.includes("definitions")) {
+        const name = getDefinitionName(type);
+
+        return name;
+      }
+
+      return type;
+  }
+};
+
+const getDefinitionProperty = (defItem: ISwaggerResultDefinitions): string => {
+  if (defItem.type !== "object") {
+    Logs.error("无法解析当前对象");
+    return "";
+  }
+
+  const props = defItem.properties;
+  const propKeys = Object.keys(props);
+
+  propKeys.forEach((key) => {
+    const name = convertType(key);
+  });
   return "";
 };
 
 export const generateDefinition = (
   definitions: IDefaultObject<ISwaggerResultDefinitions>,
 ) => {
+  const defMap = new Map<string, string>();
   // 所有定义的 key
   const definitionKeys = Object.keys(definitions);
 
   definitionKeys.forEach((key) => {
     const name = getDefinitionName(key, true);
+
+    const isExistName = defMap.has(name);
+    if (isExistName) return;
+
+    const defItem = definitions[key];
+    const props = getDefinitionProperty(defItem);
+
+    // 存储到 map 中，防止重复生成
+    defMap.set(name, props);
   });
 };
