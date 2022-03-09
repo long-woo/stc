43d264dacd9c4a2315c48037c68e148ef1a0db9b
long.woo <longwunet@outlook.com>
2022-03-09 16:07:14 +0800

    feat: 移除definition文件未使用的代码

--- src/definition.ts
+++ src/definition.ts
@@ -2,27 +2,9 @@ import Logs from "./console.ts";
 import {
   IDefaultObject,
   IDefinitionVirtualProperty,
-  ISwaggerDefinitionProperties,
   ISwaggerResultDefinitions,
 } from "./swagger.ts";
 import { caseTitle } from "./util.ts";
-
-/**
- * 生成注释
- * @param comment - 注释描述
- * @param isIndent - 缩进
- * @returns
- */
-const generateComment = (comment: string, isIndent = true) => {
-  const indent = isIndent ? "\t" : "";
-
-  return comment
-    ? `
-${indent}/**
-${indent} * ${comment}
-${indent} */`
-    : "";
-};
 
 /**
  * 获取定义的名称
@@ -59,81 +41,6 @@ const getDefinitionName = (name: string, isDefinition?: boolean): string => {
   });
 
   return newName;
-};
-
-/**
- * 转换为 TypeScript 类型
- * @param prop - 属性基础类型
- * @returns
- */
-const convertType = (prop: ISwaggerDefinitionProperties): string => {
-  // 首先判断 type 是否定义，其次判断 $ref 是否定义
-  const type = prop.type ?? prop.$ref ?? "any";
-
-  switch (type) {
-    case "integer":
-      return "number";
-    case "array": {
-      const propItem = prop.items;
-
-      if (propItem?.type) {
-        const childType = convertType(propItem);
-
-        return `${childType}[]`;
-      }
-
-      if (propItem?.$ref) {
-        const name = getDefinitionName(propItem.$ref);
-        const value = `${name}[]`;
-
-        return value;
-      }
-
-      return "any[]";
-    }
-    case "object":
-      return "IDefaultObject";
-    default: {
-      // 自定义类型
-      if (type.includes("definitions")) {
-        const name = getDefinitionName(type);
-
-        return name;
-      }
-
-      return type;
-    }
-  }
-};
-
-const getDefinitionProperty = (defItem: ISwaggerResultDefinitions): string => {
-  if (defItem.type !== "object") {
-    Logs.error("无法解析当前对象");
-    return "";
-  }
-
-  const props = defItem.properties;
-
-  const res = Object.keys(props).reduce((prev, current) => {
-    const prop = props[current];
-
-    // 必填属性
-    const isRequired = defItem.required?.includes(current);
-
-    const propName = current + (isRequired ? "" : "?");
-    const propType = convertType(prop);
-    const comment = generateComment(prop.description ?? "");
-    const enumOption = prop.enum || [];
-
-    prev.splice(
-      -1,
-      0,
-      `${comment}\n\t${propName}: ${propType}`,
-    );
-    return prev;
-  }, ["{", "\n}"]);
-
-  return res.join("");
 };
 
 /**
