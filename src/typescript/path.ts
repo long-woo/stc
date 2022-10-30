e8a8df5097347723a3db2e22b0a97216bfaa5274
long.woo <longwunet@outlook.com>
2022-10-30 20:58:54 +0800

    feat: 处理 path 的参数

--- src/typescript/path.ts
+++ src/typescript/path.ts
@@ -1,10 +1,39 @@
-import { IPathVirtualProperty } from "../swagger.ts";
+import { IPathParameter, IPathVirtualProperty } from "../swagger.ts";
+
+interface IParams {
+  path?: Array<IPathParameter>;
+  query?: Array<IPathParameter>;
+  body?: Array<IPathParameter>;
+  formData?: Array<IPathParameter>;
+}
 
 export const parserPath = (data: Map<string, IPathVirtualProperty>) => {
   data.forEach((value, key) => {
     console.log(key);
     console.log(value);
-    // const params = value.parameters.
-    `export const ${key} = ()`;
+    const _params: IParams = value.parameters.reduce(
+      (prev: IParams, current) => {
+        switch (current.category) {
+          case "path":
+            prev.path?.push(current);
+            break;
+          case "query":
+            prev.query?.push(current);
+            break;
+          case "body":
+            prev.body?.push(current);
+            break;
+          case "formData":
+            prev.formData?.push(current);
+            break;
+        }
+
+        return prev;
+      },
+      { path: [], query: [], body: [], formData: [] },
+    );
+    console.log(
+      `export const ${key} = () => webClient.${value.method}(${value.url}, ${_params})`,
+    );
   });
 };
