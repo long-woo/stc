4987452e869881bacb15a9e844709f68e91404cb
long.woo <longwunet@outlook.com>
2022-11-01 16:04:24 +0800

    feat: 解析接口参数

--- src/typescript/path.ts
+++ src/typescript/path.ts
@@ -1,7 +1,7 @@
 import { IPathParameter, IPathVirtualProperty } from "../swagger.ts";
 import { caseTitle, convertType, propCommit } from "../util.ts";
 
-interface IParams {
+interface IParamsCategory {
   header?: Array<IPathParameter>;
   path?: Array<IPathParameter>;
   query?: Array<IPathParameter>;
@@ -9,35 +9,78 @@ interface IParams {
   formData?: Array<IPathParameter>;
 }
 
+interface IPathParams {
+  name?: string;
+  def?: Array<string>;
+  ref?: string;
+  commit?: string;
+  required?: boolean;
+}
+
+/**
+ * 解析接口参数
+ * @param params - 接口参数
+ * @param key - 接口名
+ * @param category - 接口参数分类
+ * @returns
+ */
+const parserPathParams = (
+  params: IParamsCategory,
+  key: string,
+  category: "header" | "path" | "query" | "body" | "formData",
+) =>
+  params[category]?.reduce(
+    (prev: IPathParams, current, index, arr) => {
+      let _item = `${current.name}${current.required ? "" : "?"}: ${
+        convertType(current.type, current.ref)
+      }`;
+
+      if (arr.length === 1) {
+        prev = {
+          name: current.name,
+          commit: current.description ?? current.category,
+          ref: _item,
+          required: current.required,
+        };
+      } else {
+        _item = `${propCommit(current.description ?? "")}${_item}`;
+
+        if (index === 0) {
+          const _defName = `${caseTitle(key)}${
+            caseTitle(current.category)
+          }Params`;
+
+          prev = {
+            name: current.category,
+            commit: current.category,
+            ref: `${current.category}: ${_defName}`,
+            def: [
+              `export interface ${_defName} {`,
+              _item,
+              "}",
+            ],
+          };
+        } else {
+          prev.def?.splice(prev.def.length - 1, 0, _item);
+        }
+      }
+
+      return prev;
+    },
+    {},
+  );
+
+/**
+ * 解析接口
+ * @param data - 接口信息
+ */
 export const parserPath = (data: Map<string, IPathVirtualProperty>) => {
   data.forEach((value, key) => {
     console.log(key);
     console.log(value);
     const _params = value.parameters.reduce(
-      (prev: IParams, current) => {
-        // const _item = `${propCommit(current.description ?? "")}${current.name}${
-        //   current.required ? "" : "?"
-        // }: ${convertType(current.type, current.ref)}`;
-
-        // if (!prev[current.category]?.length) {
-        //   prev[current.category]?.push(
-        //     // `interface ${caseTitle(key)}${caseTitle(current.category)}Params {`,
-        //     _item,
-        //     // "}",
-        //   );
-        // }
-        // // else {
-        // //   prev[current.category]?.splice(
-        // //     (prev[current.category]?.length ?? 0) - 1,
-        // //     0,
-        // //     _item,
-        // //   );
-        // // }
+      (prev: IParamsCategory, current) => {
         prev[current.category]?.push(current);
-        // prev[current.category]?.push(
-        //   _item,
-        // );
-
         return prev;
       },
       {
@@ -48,14 +91,28 @@ export const parserPath = (data: Map<string, IPathVirtualProperty>) => {
         formData: [],
       },
     );
-    console.log(_params);
+
+    const _header = parserPathParams(_params, key, "header");
+    const _path = parserPathParams(_params, key, "path");
+    const _query = parserPathParams(_params, key, "query");
+    const _body = parserPathParams(_params, key, "body");
+    const _formData = parserPathParams(_params, key, "formData");
+
+    const _paramsDef = [
+      _path?.ref,
+      _body?.ref,
+      _formData?.ref,
+      _query?.ref,
+      _header?.ref,
+    ].filter((item) => item);
+
     const _responseDef = convertType(
       value.response.type ?? "",
       value.response.ref,
     );
-    // path/query/body/formData - id:number or {id: number, name: string}
+
     console.log(
-      `export const ${key} = () => webClient.${value.method}<${_responseDef}>('${value.url}')`,
+      `export const ${key} = (${_paramsDef.toString()}) => webClient.${value.method}<${_responseDef}>('${value.url}')`,
     );
   });
 };
