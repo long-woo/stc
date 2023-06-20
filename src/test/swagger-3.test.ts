57791967a5c70dc05ce35c6e23ff14dd3394ba39
loong.woo <longwunet@outlook.com>
2023-06-20 22:14:28 +0800

    feat: 增加测试

--- src/test/swagger-3.test.ts
+++ src/test/swagger-3.test.ts
@@ -1,2 +1,20 @@
 // https://petstore3.swagger.io/api/v3/openapi.json
-// import { assert } from "std/testing/asserts.ts";
+import { assert, assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("测试-在命令行中", async () => {
+  const command = new Deno.Command("deno", {
+    args: [
+      "run",
+      "-A",
+      "src/mod.ts",
+      "--url=https://petstore3.swagger.io/api/v3/openapi.json",
+      "--outDir=out",
+    ],
+  });
+
+  const { code, stdout, stderr } = await command.output();
+  // console.assert(code === 0);
+  // console.assert("hello\n" === new TextDecoder().decode(stdout));
+  // console.assert("world\n" === new TextDecoder().decode(stderr));
+  assertEquals(0, code);
+});
