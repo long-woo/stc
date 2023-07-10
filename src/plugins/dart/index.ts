1a3ee99fed2f7df120b012adbb9fc07599203a01
loong.woo <longwunet@outlook.com>
2023-07-10 15:14:02 +0800

    chore: 注释 dart 插件支持

--- src/plugins/dart/index.ts
+++ src/plugins/dart/index.ts
@@ -1,15 +1,15 @@
-import { ISwaggerOptions } from "../../swagger.ts";
-import { IPlugin } from "../typeDeclaration.ts";
+// import { ISwaggerOptions } from "../../swagger.ts";
+// import { IPlugin } from "../typeDeclaration.ts";
 
 /**
  * dart 插件。
  * 依赖 [dio](https://github.com/cfug/dio)
  */
-export const dartPlugin: IPlugin = {
-  name: "stc:DartPlugin",
-  setup(options: ISwaggerOptions) {
-  },
-  onTransform(def, action) {
-    return {};
-  },
-};
+// export const dartPlugin: IPlugin = {
+//   name: "stc:DartPlugin",
+//   setup(options: ISwaggerOptions) {
+//   },
+//   onTransform(def, action) {
+//     return {};
+//   },
+// };
