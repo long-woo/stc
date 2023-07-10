7b89dc0c1bb6bee5a4eaee876c39640db82bb1cd
loong.woo <longwunet@outlook.com>
2023-07-10 17:39:03 +0800

    fix: 修复转换 typescript 类型问题

--- src/util.ts
+++ src/util.ts
@@ -80,8 +80,8 @@ export const getRefType = (ref: string) =>
  * @returns
  */
 export const convertType = (type: string, ref?: string) => {
-  // 当只有 ref 的时候，直接返回 ref
-  if (!type && ref) return ref;
+  // 当只有 ref 或者 type 为 object 时，直接返回 ref
+  if ((!type || type === "object") && ref) return ref;
 
   // 若 type 与 ref 相等，则表示为自定义类型
   if (type === ref) return type || "unknown";
