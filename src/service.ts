c0d1bfc1ac00f7b6d416575ab14f3363ee945dc7
loong.woo <longwunet@outlook.com>
2024-04-12 16:20:30 +0800

    feat: 增加 service

---
+++ src/service.ts
@@ -0,0 +1,24 @@
+import { Eta } from "x/eta@v3.4.0/src/index.ts";
+
+const createFetchRuntimeFile = (lang = "ts", platform = "axios") => {
+  const eta = new Eta({ views: "./src/plugins/typescript/shared" });
+
+  const _res = eta.render("./fetchRuntime", {
+    platform,
+  });
+
+  // createFile(`${options.outDir}/shared/fetchRuntime.${options.lang}`, _res);
+  return _res;
+};
+
+Deno.serve((_req: Request) => {
+  const _url = new URL(_req.url);
+
+  if (_url.pathname.includes("/api/")) {
+    const _res = createFetchRuntimeFile();
+
+    return new Response(_res);
+  }
+
+  return new Response("stc service is running!");
+});
