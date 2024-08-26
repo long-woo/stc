859160d6ed6c710e8aa4dc2779ec7392544bcadd
loong.woo <longwunet@outlook.com>
2024-08-26 16:21:20 +0800

    chore: 重新生成 deno.lock

--- src/plugins/javascript/test/index.test.ts
+++ src/plugins/javascript/test/index.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "std/assert/mod.ts";
+import { assertEquals } from "@std/assert";
 
 Deno.test("JavaScript 插件", async () => {
   const command = new Deno.Command("deno", {
