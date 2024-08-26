7dee6f38324fb0b708bec488daedfda648798533
loong.woo <longwunet@outlook.com>
2024-08-26 15:21:27 +0800

    fix(plugin): 修复 js 插件卡住问题

--- README.md
+++ README.md
@@ -179,7 +179,7 @@ For convenience, STC can not only develop plugins in Deno, but also provides `@l
 
 ```ts
 // 引用模块
-import { start } from 'https://deno.land/x/stc@1.6.1/mod.ts'
+import { start } from 'https://deno.land/x/stc@1.6.4/mod.ts'
 
 // 定义插件
 const myPlugin: IPlugin = {
---
+++ src/plugins/javascript/test/index.test.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from "std/assert/mod.ts";
+
+Deno.test("JavaScript 插件", async () => {
+  const command = new Deno.Command("deno", {
+    args: [
+      "run",
+      "-A",
+      "src/main.ts",
+      "--url=https://petstore3.swagger.io/api/v3/openapi.json",
+      "--lang=js",
+    ],
+  });
+
+  const { code } = await command.output();
+
+  assertEquals(0, code);
+});
